3778224957ac608d86b301cbc4b74496c123e4fe
Maincoke <maincoke@gmail.com>
2018-05-24 23:13:32 -0500

    Reordenamiento de codigo para establecer y unir el modulo general

--- js/app.js
+++ js/app.js
@@ -1,61 +1,4 @@
-// ---> Funcion que calcula las operaciones aritmeticas de la calculadora <--- //
-function calcularOp(numop1, numop2, opnum) {
-    var resultop = 0;
-    if (numop2 != 0 || numop1 != 0) {
-        switch (opnum) {
-            case 1:
-                resultop = parseFloat(numop1 * numop2);
-                break;
-            case 2:
-                resultop = parseFloat(numop1 + numop2);
-                break;
-            case 3:
-                resultop = parseFloat(numop1 - numop2);
-                break;
-            case 4:
-                resultop = parseFloat(numop1 / numop2);
-                break;
-            case 5:
-                resultop = parseFloat(Math.sqrt(numop1));
-        }
-    }
-    return resultop;
-}
-// ---> Funciones de cambio y actualización de Pantalla <--- //
-function verficaPant(refpant) {
-    if (document.getElementById('display').innerHTML.length == 1 && document.getElementById('display').innerHTML == '0') {
-        refpant = '';
-    }
-    return refpant;
-}
-
-function cambiarPant(refpant) {
-    if (document.getElementById('display').innerHTML.length <= 9) {
-        document.getElementsByTagName('span')[1].innerHTML = refpant;
-        if (refpant == 'E') {
-            refpant = '';
-        }
-    }
-    if (refpant.includes("'")) {
-        refpant = refpant.substr(0, refpant.length - 1);
-    }
-    return refpant;
-}
-// ---> Funciones para el efecto de tecla presionada en calculadora (Hacer click en tecla) <--- //
-function efectoTecla(tecla) {
-    tecla.style.padding = "2px";
-    tecla.style.background = "#858080";
-    tecla.style.border = "solid 2px #858080";
-    tecla.style.borderRadius = "15px";
-}
-
-function efectoTeclaSoltar(tecla) {
-    tecla.style.padding = "0px";
-    tecla.style.background = "";
-    tecla.style.border = "";
-    tecla.style.borderRadius = "0px";
-}
-// ---> Objeto-Modulo que realiza la operatividad y funcionalidad de la Calculadora <--- //
+// **++--// ---> Objeto-Modulo que realiza la operatividad y funcionalidad de la Calculadora <--- //--++** //
 var Calculadora = {
     verPant: '',
     primerOp: 0,
@@ -72,6 +15,29 @@ var Calculadora = {
         document.onkeypress = this.verificaTecla;
         this.asignaClick('img');
     },
+    // ---> Funcion que calcula las operaciones aritmeticas de la calculadora <--- //
+    calcularOp: function(numop1, numop2, opnum) {
+        var resultop = 0;
+        if (numop2 != 0 || numop1 != 0) {
+            switch (opnum) {
+                case 1:
+                    resultop = parseFloat(numop1 * numop2);
+                    break;
+                case 2:
+                    resultop = parseFloat(numop1 + numop2);
+                    break;
+                case 3:
+                    resultop = parseFloat(numop1 - numop2);
+                    break;
+                case 4:
+                    resultop = parseFloat(numop1 / numop2);
+                    break;
+                case 5:
+                    resultop = parseFloat(Math.sqrt(numop1));
+            }
+        }
+        return resultop;
+    },
     // ---> Funcion Nativa que verifica los operandos en cada operacion aritmetica <--- //
     verficarOp: function(duvez) {
         if ((verPant.length >= 1 && parseFloat(verPant) != 0) && (operacion == 0 && primerOp.toString().length <= 9 && !duvez)) {
@@ -84,28 +50,47 @@ var Calculadora = {
             primerOp = parseFloat(verPant);
         }
     },
+    // ---> Funciones de cambio y actualización de la Pantalla de la Calculadora <--- //
+    verficaPant: function(refpant) {
+        if (document.getElementById('display').innerHTML.length == 1 && document.getElementById('display').innerHTML == '0') {
+            refpant = '';
+        }
+        return refpant;
+    },
+    cambiarPant: function(refpant) {
+        if (document.getElementById('display').innerHTML.length <= 9) {
+            document.getElementsByTagName('span')[1].innerHTML = refpant;
+            if (refpant == 'E') {
+                refpant = '';
+            }
+        }
+        if (refpant.includes("'")) {
+            refpant = refpant.substr(0, refpant.length - 1);
+        }
+        return refpant;
+    },
     // **** // ---> Funciones del evento Click de las Teclas de la Calculadora <--- // **** //
     // ---> Tecla Cero(On/C): Funcion que reinicia y coloca en Cero la Calculadora <--- //
     ceroReinico: function() {
         verPant = '0';
         primerOp = 0, segundOp = 0, operacion = 0, otvz = 0;
         document.getElementsByTagName('span')[1].innerHTML = verPant;
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
     // ---> Tecla Punto(.): Funcion que coloca el punto decimal y verifica si ya existe el punto en cifra operando <--- //
     puntoDecimal: function() {
-        verPant = verficaPant(verPant);
+        verPant = Calculadora.verficaPant(verPant);
         if (!verPant.includes('.')) {
             if (verPant == '' || verPant == '-') {
                 verPant = '0';
             }
             verPant = verPant + '.';
         }
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
     // ---> Tecla Signo(+/-): Funcion que asigna y retira el signo negativo en la cifra operando <--- //
     signoNumero: function() {
-        verPant = verficaPant(verPant);
+        verPant = Calculadora.verficaPant(verPant);
         if (Math.sign(parseFloat(verPant)) == -1 && parseFloat(verPant) < 0) {
             verPant = Math.abs(verPant).toString();
         } else if (parseFloat(verPant) > 0) {
@@ -113,15 +98,15 @@ var Calculadora = {
         } else {
             verPant = '-';
         }
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
     // ---> Teclas Numericas(0) al (9): Funcion que agrega los numeros del 0 al 9 en la cifra operando <--- //
     teclaNumClick: function(clkTecla) {
-        verPant = verficaPant(verPant);
+        verPant = Calculadora.verficaPant(verPant);
         if ((clkTecla.target.id >= 0 && clkTecla.target.id <= 9) && (parseFloat(verPant) != 0 || verPant.includes('.')) && verPant.length < 9) {
             verPant = verPant + clkTecla.target.id.toString();
         }
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
     /* / ---> Tecla Multiplicar(x) > Sumar(+) > Restar(-) > Dividir(:) > Raiz Cuadrada(\/¨):
             Funcion que se ejecuta en cada una de las teclas de operaciones aritmeticas <--- / */
@@ -150,7 +135,7 @@ var Calculadora = {
         } else {
             verPant = ''
         }
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
     // ---> Tecla Igual(=): Funcion que ejecuta la operacion aritmetica y proporciona el resultado de la operacion <--- //
     // Realiza ejecucion y verificacion de operacion Flash(Rapida) y Repeat(Repite operacion con 2do operando) //
@@ -160,7 +145,7 @@ var Calculadora = {
             if (segundOp == 0) {
                 Calculadora.verficarOp(true);
             }
-            resultado = calcularOp(primerOp, segundOp, operacion);
+            resultado = Calculadora.calcularOp(primerOp, segundOp, operacion);
             if (resultado < -9999999 || resultado > 99999999) {
                 verPant = 'E' + (parseFloat(resultado.toFixed(7)).toString()).substr(0, 8) + "'";
                 primerOp = 0, segundOp = 0, operacion = 0;
@@ -168,21 +153,27 @@ var Calculadora = {
                 verPant = (parseFloat(resultado.toFixed(7)).toString()).substr(0, 9) + "'";
             }
         }
-        verPant = cambiarPant(verPant);
+        verPant = Calculadora.cambiarPant(verPant);
     },
-    // ---> Funciones de asignacion que llaman a las funciones de efecto de las Teclas de la Calculadora <--- //
-    fxClickDown: function(clkTecla) {
-        efectoTecla(clkTecla.target);
+    // ---> Funciones para el efecto de tecla presionada en las Teclas de la Calculadora (Hacer click en tecla) <--- //
+    efectoTecla: function(tecla) {
+        tecla.target.style.padding = "2px";
+        tecla.target.style.background = "#858080";
+        tecla.target.style.border = "solid 2px #858080";
+        tecla.target.style.borderRadius = "15px";
     },
-    fxClickUp: function(clkTecla) {
-        efectoTeclaSoltar(clkTecla.target);
+    efectoTeclaSoltar: function(tecla) {
+        tecla.target.style.padding = "0px";
+        tecla.target.style.background = "";
+        tecla.target.style.border = "";
+        tecla.target.style.borderRadius = "0px";
     },
     // ---> Funcion que realiza la asignacion de la funciones de cada tecla de la Calculadora <--- //
     asignaClick: function(e_tecla) {
         var tecla = document.getElementsByTagName(e_tecla);
         for (i = 0; i < tecla.length; i++) {
-            tecla[i].onmousedown = this.fxClickDown;
-            tecla[i].onmouseup = this.fxClickUp;
+            tecla[i].onmousedown = this.efectoTecla;
+            tecla[i].onmouseup = this.efectoTeclaSoltar;
             switch (tecla[i].id) {
                 case 'on':
                     tecla[i].onclick = this.ceroReinico;
@@ -212,7 +203,7 @@ var Calculadora = {
             Operacion aritmetica (excepto Raiz Cuadrada), Punto Decimal y resultado con -Enter- (<--|) <---/ */
     verificaTecla: function(e_tecla) {
         var teclaPres = e_tecla.which || e_tecla.keyCode;
-        verPant = verficaPant(verPant);
+        verPant = Calculadora.verficaPant(verPant);
         if (teclaPres == 13 || (teclaPres >= 42 && teclaPres <= 57) || (teclaPres >= 42 && teclaPres <= 43)) {
             if (teclaPres == 13) {
                 Calculadora.igualClick();
@@ -247,10 +238,11 @@ var Calculadora = {
                 }
             }
             if (teclaPres != 13) {
-                verPant = cambiarPant(verPant);
+                verPant = Calculadora.cambiarPant(verPant);
             }
         }
     }
 }
 
-Calculadora.iniciarCalc(); // ---> Operacion principal y ejecucion de Inicio de la Calculadora <--- //
+// ---> Operacion principal y ejecucion de Inicio de la Calculadora <--- //
+Calculadora.iniciarCalc();
